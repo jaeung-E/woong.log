281514b76046791911a79ce8849ac90831c6db75
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-02-09 03:47:22 +0900

    feat: TagList 컴포넌트에 selectedTag prop 추가

--- src/components/TagList.tsx
+++ src/components/TagList.tsx
@@ -2,14 +2,19 @@ import TagItem from "./TagItem";
 
 interface Props {
   tags: string[];
+  selectedTag?: string;
   isActivate?: boolean;
 }
 
-function TagList({ tags, isActivate = false }: Props) {
+function TagList({ tags, selectedTag, isActivate = false }: Props) {
   return (
     <div className="flex flex-wrap justify-center gap-x-2 gap-y-4">
       {tags.map((tag) => (
-        <TagItem name={tag} isActivate={isActivate} />
+        <TagItem
+          name={tag}
+          isSelected={selectedTag === tag}
+          isActivate={isActivate}
+        />
       ))}
     </div>
   );

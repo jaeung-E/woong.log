cdbc6324bf5df088a2c3441d756128a78975aa05
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-02-09 21:24:11 +0900

    design: TagItem selected 색상 변경

--- src/components/TagItem.tsx
+++ src/components/TagItem.tsx
@@ -18,7 +18,7 @@ function TagItem({ name, isSelected = false, isActivate = false }: Props) {
       <ConditionalLink link={isActivate ? `/tags/${name}` : ""}>
         <div
           className={`flex rounded-full text-sm px-3 py-1 border-solid border-slate-400 border ${
-            isSelected ? "bg-green-400" : ""
+            isSelected ? "bg-blue-100" : ""
           } ${
             !isSelected && isActivate
               ? "hover:bg-slate-100 transition-colors duration-300"

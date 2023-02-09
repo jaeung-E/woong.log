b0b78c2acffe820744631f842f1de0c0aed90a0e
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-02-09 22:48:31 +0900

    feat: PostItem createDate 추가

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -6,21 +6,28 @@ interface Props {
 
 function PostItem({ post }: Props) {
   const { slug } = post;
-  const { title, description, thumbnail } = post.data;
+  const { title, description, thumbnail, createDate } = post.data;
 
   return (
     <div className="py-4 transition-colors duration-300 hover:text-blue-400">
       <a href={`/posts/${slug}`}>
         <div className="grid grid-cols-12">
-          <div className="col-span-3 col-start-1 row-start-1 row-end-3">
+          <div className="col-span-3 row-span-2">
             <img
               src={`${thumbnail}`}
               alt="thumbnail"
               className="object-cover w-full h-full"
             />
           </div>
-          <h1 className="col-span-9 col-start-5 text-2xl font-bold">{title}</h1>
-          <div className="col-span-9 col-start-5 py-4">
+          <div className="flex items-end col-start-5 col-span-6">
+            <h1 className="text-2xl font-bold">{title}</h1>
+          </div>
+          <div className="flex items-end justify-end col-span-2 pr-3">
+            <span className="text-sm !text-gray-400">
+              {createDate.toLocaleDateString("ko-KR")}
+            </span>
+          </div>
+          <div className="col-start-5 col-span-9  py-4">
             <p className="break-words overflow-hidden h-12 !text-gray-400">
               {description}
             </p>

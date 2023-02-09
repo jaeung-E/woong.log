69a682aa35001edb24ef99aeee1c17a77d0f0ac7
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-02-09 15:39:49 +0900

    design: TagItem close 버튼 margin 추가

--- src/components/TagItem.tsx
+++ src/components/TagItem.tsx
@@ -29,7 +29,7 @@ function TagItem({ name, isSelected = false, isActivate = false }: Props) {
             {name}
           </span>
           {isSelected && (
-            <button onClick={handleClick}>
+            <button onClick={handleClick} className="ml-1">
               <img
                 src="/assets/icon/close.svg"
                 alt="close"

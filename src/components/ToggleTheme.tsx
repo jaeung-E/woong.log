32adfb48083a15598c8f4bfa366d5766e52d617a
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-02-13 22:02:08 +0900

    refactor: ToggleTheme Fragment 제거

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -28,22 +28,20 @@ function ToggleTheme() {
   };
 
   return (
-    <>
+    <div
+      className={`flex items-center p-0.5 cursor-pointer w-16 h-7 rounded-full ${
+        toggle ? "bg-black" : "bg-blue-400"
+      }`}
+      onClick={handleToggle}
+    >
       <div
-        className={`flex items-center p-0.5 cursor-pointer w-16 h-7 rounded-full ${
-          toggle ? "bg-black" : "bg-blue-400"
+        className={`w-6 h-6 bg-white rounded-full transition-transform duration-300 ${
+          toggle ? `translate-x-[calc(100%+0.75rem)]` : ""
         }`}
-        onClick={handleToggle}
       >
-        <div
-          className={`w-6 h-6 bg-white rounded-full transition-transform duration-300 ${
-            toggle ? `translate-x-[calc(100%+0.75rem)]` : ""
-          }`}
-        >
-          {toggle ? <Moon className="w-6 h-6" /> : <Sun className="w-6 h-6" />}
-        </div>
+        {toggle ? <Moon className="w-6 h-6" /> : <Sun className="w-6 h-6" />}
       </div>
-    </>
+    </div>
   );
 }
 

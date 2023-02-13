23e65c918ec330e0e60bb0b8284656f1bc757d89
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-02-13 21:33:56 +0900

    feat: Layout Footer 추가

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="p-8 text-center text-stone-400 text-sm">
+      <span>© 2023 JaeungE.</span>
+    </footer>
+  );
+}
+
+export default Footer;
